1cbfce1feb314e1d0e3173556e9ded3f6732573b
tushermahmud <sazzadmahmud01795355849@gmail.com>
2022-09-23 14:55:45 +0530

    The project is initialized and structured

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//app middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
+//app middlewares
+
+//app routers
+app.get("/", (req, res) => {
+  res.send("welcome to my kingdom");
+});
+//app routers
+
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(
+    "mongodb+srv://tusher:sazzadkueturp2k14@cluster0-jaavp.mongodb.net/hr-hero?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    app.listen(PORT, (req, res) => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
